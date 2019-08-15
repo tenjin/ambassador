f3e04f283e14a4714a88ed2acecaee45ccad4a11
Jonathan Suever <suever@gmail.com>
2019-08-15 15:55:24 -0400

    Add filter at the correct level

--- ambassador/ambassador/envoy/v2/v2listener.py
+++ ambassador/ambassador/envoy/v2/v2listener.py
@@ -508,9 +508,9 @@ class V2Listener(dict):
 
             # Use sane access log spec in JSON
             if(config.ir.ambassador_module.envoy_log_type.lower() == "json") :
-                self.access_log = [ {
+                self.access_log = [ dict({
                     'name': 'envoy.file_access_log',
-                    'config': dict({
+                    'config': {
                         'path': config.ir.ambassador_module.envoy_log_path,
                         'json_format': {
                             'start_time': '%START_TIME%',
@@ -536,17 +536,17 @@ class V2Listener(dict):
                             'istio_policy_status': '%DYNAMIC_METADATA(istio.mixer:status)%',
                             'upstream_transport_failure_reason': '%UPSTREAM_TRANSPORT_FAILURE_REASON%'
                         }
-                    }, **params)
-                } ]
+                    }
+                }, **params) ]
             else:
                 # Use a sane access log spec
-                self.access_log = [ {
+                self.access_log = [ dict({
                     'name': 'envoy.file_access_log',
-                    'config': dict({
+                    'config': {
                         'path': config.ir.ambassador_module.envoy_log_path,
                         'format': 'ACCESS [%START_TIME%] \"%REQ(:METHOD)% %REQ(X-ENVOY-ORIGINAL-PATH?:PATH)% %PROTOCOL%\" %RESPONSE_CODE% %RESPONSE_FLAGS% %BYTES_RECEIVED% %BYTES_SENT% %DURATION% %RESP(X-ENVOY-UPSTREAM-SERVICE-TIME)% \"%REQ(X-FORWARDED-FOR)%\" \"%REQ(USER-AGENT)%\" \"%REQ(X-REQUEST-ID)%\" \"%REQ(:AUTHORITY)%\" \"%UPSTREAM_HOST%\"\n'
-                    }, **params)
-                } ]
+                    }
+                }, **params) ]
 
             # Assemble filters
             for f in config.ir.filters:
